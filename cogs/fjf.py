e1346297a87632c692595ac0124e1a8866a38cea
AidenBeresford <aidenberes@gmail.com>
2023-01-23 23:51:22 -0600

    Optimized Four Job Fiesta cog

--- cogs/fjf.py
+++ cogs/fjf.py
@@ -7,8 +7,6 @@ water_jobs = ('red mage', 'time mage', 'summoner', 'berserker', 'mystic knight')
 fire_jobs = ('ninja', 'hunter', 'geomancer', 'beastmaster', 'bard')
 earth_jobs = ('dragoon', 'samurai', 'dancer', 'chemist')
 
-current_jobs = ['Wind', 'Water', 'Fire', 'Earth']
-
 random.seed()
 
 
@@ -18,23 +16,19 @@ class FJF(commands.Cog):
 
     @discord.slash_command()
     async def wind(self, ctx):
-        current_jobs[0] = wind_jobs[random.randrange(0, len(wind_jobs)-1)]
-        await ctx.respond("Wind: " + current_jobs[0].title())
+        await ctx.respond('Wind: ' + wind_jobs[random.randrange(0, len(wind_jobs)-1)])
 
     @discord.slash_command()
     async def water(self, ctx):
-        current_jobs[1] = water_jobs[random.randrange(0, len(water_jobs) - 1)]
-        await ctx.respond("Water: " + current_jobs[1].title())
+        await ctx.respond('Water: ' + water_jobs[random.randrange(0, len(water_jobs) - 1)])
 
     @discord.slash_command()
     async def fire(self, ctx):
-        current_jobs[1] = fire_jobs[random.randrange(0, len(fire_jobs) - 1)]
-        await ctx.respond("Fire: " + current_jobs[2].title())
+        await ctx.respond('Fire: ' + fire_jobs[random.randrange(0, len(fire_jobs) - 1)])
 
     @discord.slash_command()
     async def earth(self, ctx):
-        current_jobs[1] = earth_jobs[random.randrange(0, len(earth_jobs) - 1)]
-        await ctx.respond("Earth: " + current_jobs[3].title())
+        await ctx.respond('Earth: ' + earth_jobs[random.randrange(0, len(earth_jobs) - 1)])
 
 
 def setup(bot):
